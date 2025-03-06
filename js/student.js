d5bacb83ce8024a4f3030559a26cca6c3604b138
CarlosDanielArauzSanjuan <darauzing@gmail.com>
2025-03-06 12:34:55 -0500

    feat: ✨ student form with all features completed

---
+++ js/student.js
@@ -0,0 +1,26 @@
+import {save, transformInputFormulary, loadStudent, showRowsTable} from "./formStudent.js";
+
+const add__student = document.querySelector("#add__student");
+const dialog__add_student = document.querySelector("#dialog__add_student");
+const dialog__close = document.querySelector("#dialog__close");
+const form__student = document.querySelector("#form__student");
+const table__student = document.querySelector("#table__student")
+
+
+showRowsTable(loadStudent());
+
+
+
+add__student.addEventListener("click", ()=>{
+    dialog__add_student.showModal();
+})
+
+dialog__close.addEventListener("click", ()=> dialog__add_student.close());
+
+form__student.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const data = transformInputFormulary(e)
+    const response = save(data);
+    console.log(response);
+    form__student.reset();
+})
