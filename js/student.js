c2f7693ac77f5fd8d89b5d05a59f3395326ab295
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2025-03-09 23:44:59 -0500

    feat: :construction: work in progress

--- js/student.js
+++ js/student.js
@@ -1,81 +1,58 @@
-import {enableFromStudent} from "./handler.js";
+import {save,edit, transformInputFormulary, loadStudent, showRowsTable} from "./formStudent.js";
 
-export const loadStudent = ()=>{
-    const DB = (localStorage.getItem("students")) ? JSON.parse(localStorage.getItem("students")) : [];
-    return DB;
-}
-export const save = (data)=>{
+const add__student = document.querySelector("#add__student");
+const dialog__add_student = document.querySelector("#dialog__add_student");
+const dialog__student_edit = document.querySelector("#dialog__student_edit");
+const dialog__close = document.querySelector("#dialog__close");
+const dialog__close_edit = document.querySelector("#dialog__close_edit");
+const dialog__student_delete = document.querySelector("#dialog__student_delete");
+const form__student = document.querySelector("#form__student");
+const form__student_edit = document.querySelector("#form__student_edit");
+const form__student_delete = document.querySelector("#form__student_delete");
+const table__student = document.querySelector("#table__student")
+
+
+addEventListener("DOMContentLoaded", (e)=>{
     const DB = loadStudent();
-    DB.push(data);
-    showRowsTable([data]);
-    localStorage.setItem("students", JSON.stringify(DB));
-    return {status: 201, message: `El estudiante ${data.name} fue registrado exitosamente.`}
-}
-export const edit = (data)=>{
-    const DB = loadStudent(); // [{name: "Miguel"},{name "Juan"}]
-    const {id} = data;  // {id: 1, name:"JUAN"} - const id = 1
-    delete data.id; // {name:"JUAN"}
-    DB[id] = data; // {name "Juan"} = {name:"JUAN"}
-    localStorage.setItem("students", JSON.stringify(DB));
-    const row = table__student.querySelectorAll("tr")[id];
-    row.children[1].textContent = data.name;
-    row.children[2].textContent = data.email;
-    row.children[3].textContent = data.phone;
-    row.children[4].textContent = data.enroll_number;
-}
+    showRowsTable(DB);
+})
 
-export const transformInputFormulary =  (e)=>{
-    const data = Object.fromEntries(new FormData(e.target));
-    data.phone = Number(data.phone)
-    data.enroll_number = Number(data.enroll_number)
-    data.date_of_admission = new Date().toISOString();
-    return data;
-}
-export const showRowsTable = (DB)=>{
-    for (let i = 0; i < DB.length; i++) {
-        const tr = document.createElement("tr")
-        const tdImage = document.createElement("td");
-        const imgStudent = document. createElement("img");
-        imgStudent.src = "#"
-        imgStudent.alt = "student";
-        tdImage.append(imgStudent);
-        const tdName = document.createElement("td");
-        tdName.textContent = DB[i].name;
-        const tdEmail = document.createElement("td");
-        tdEmail.textContent = DB[i].email;
-        const tdPhone = document.createElement("td");
-        tdPhone.textContent = DB[i].phone;
-        const tdEnrollNumber = document.createElement("td");
-        tdEnrollNumber.textContent = DB[i].enroll_number;
-        const tdDate_of_admission = document.createElement("td");
-        tdDate_of_admission.textContent = DB[i].date_of_admission;
-        const tdActions = document.createElement("td");
-        const spanEdit = document.createElement("span");
-        spanEdit.classList.add("span__edit");
-        spanEdit.textContent = "✏️";
-        spanEdit.dataset.id_student = i;
-        spanEdit.addEventListener("click", enableFromStudent)
 
-        const spanDelete = document.createElement("span");
-        spanDelete.classList.add("span__delete");
-        spanDelete.textContent = "🗑️";
+add__student.addEventListener("click", ()=>{
+    dialog__add_student.showModal();
+})
 
-        tdActions.append(spanEdit, spanDelete);
-        tr.append(tdImage, tdName, tdEmail, tdPhone, tdEnrollNumber, tdDate_of_admission, tdActions);
-        table__student.append(tr);
+dialog__close.addEventListener("click", ()=> dialog__add_student.close());
+dialog__close_edit.addEventListener("click", ()=> dialog__student_edit.close());
+
+form__student.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const data = transformInputFormulary(e)
+    const response = save(data);
+    console.log(response);
+    form__student.reset();
+})
+form__student_edit.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const data = transformInputFormulary(e)
+    const response = edit(data);
+    console.log(response);
+    form__student_edit.reset();
+    
+})
+
+form__student_delete.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const actions = e.submitter.dataset.action; // delete o close
+    if(actions == "close") dialog__student_delete.close();
+    if(actions == "delete"){
+        const data = Object.fromEntries(new FormData(e.target));
+        const DB = loadStudent();
+        DB.splice(data.id, 1);
+        localStorage.setItem("students", JSON.stringify(DB));
+        table__student.innerHTML = "";
+        showRowsTable(DB);
+        form__student_delete.reset();
+        dialog__student_delete.close();
     }
-
-    //         <tr>
-    //             <td><img src="#" alt="student"></td>
-    //             <td>Karthi</td>
-    //             <td>karthi@gmmail.com</td>
-    //             <td>7305477760</td>
-    //             <td>1234567305477760</td>
-    //             <td>08-Dec, 2021</td>
-    //             <td class="td__actions">
-    //                 <span class="span__edit">✏️</span>
-    //                 <span class="span__delete">🗑️</span>
-    //             </td>
-    //         </tr>
-
-}
+})
